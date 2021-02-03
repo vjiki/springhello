663a6840d6b7a07eb933dd82e93e2c660e1faadd
vjiki <nezzabudka5@gmail.com>
2021-02-03 17:52:30 +0300

    xml spring

--- src/main/java/com/github/vjiki/springhello/SpringHello.java
+++ src/main/java/com/github/vjiki/springhello/SpringHello.java
@@ -1,7 +1,16 @@
 package com.github.vjiki.springhello;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class SpringHello {
     public static void main(String[] args) {
-        System.out.println("Hello, Autocode!");
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        PersonBean person = context.getBean("iam", PersonBean.class);
+
+        System.out.println(person.getName() + " " + person.getAge());
+
+        context.close();
     }
 }
